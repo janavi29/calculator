5b3fb7b79eb4da131b46230f56df863a3e94a094
janavi29 <114308399+janavi29@users.noreply.github.com>
2022-11-02 17:51:18 +0530

    Initial commit

---
+++ src/calculator.js
@@ -0,0 +1,12 @@
+// Import React (Mandatory Step).
+import React from 'react';
+
+// Create a Class Component Calculator.
+class Calculator extends React.Component {
+  render() {
+    return 'GeeksforGeeks Calculator';
+  }
+}
+
+// Export Calculator.
+export default Calculator;
